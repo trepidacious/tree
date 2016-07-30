7eed83896e54fd5dac4c1de3f26b95bfc644016b
trepidacious <trepidacious@gmail.com>
2016-07-30 13:45:26 +0100

    Remove some more boilerplate with view function

--- src/main/scala/org/rebeam/tree/demo/DemoApp.scala
+++ src/main/scala/org/rebeam/tree/demo/DemoApp.scala
@@ -49,46 +49,39 @@ object Employee {
 
 object DemoApp extends JSApp {
 
+  implicit def cursorReuse[A]: Reusability[Cursor[A]] = Reusability.by_==
+
+  def view[A](name: String, overlay: Boolean = true)(render: Cursor[A] => ReactElement) = if (overlay) {
+    ReactComponentB[Cursor[A]](name).render_P(render).configure(Reusability.shouldComponentUpdateWithOverlay).build
+  } else {
+    ReactComponentB[Cursor[A]](name).render_P(render).build
+  }
+
   def main(): Unit = {
 
-    implicit def cursorReuse[A]: Reusability[Cursor[A]] = Reusability.by_==
+    val StringView = view[String]("StringView") { c =>
+      <.input(
+        ^.`type` := "text",
+        ^.value := c.model,
+        ^.onChange ==> ((e: ReactEventI) => c.set(e.target.value))
+      )
+    }
 
-    val StringView =
-      ReactComponentB[Cursor[String]]("StringView")
-        .render_P(c =>
-          <.input(
-            ^.`type` := "text",
-            ^.value := c.model,
-            ^.onChange ==> ((e: ReactEventI) => c.set(e.target.value))
-          )
-        )
-        .configure(Reusability.shouldComponentUpdateWithOverlay)
-        .build
+    val IntView = view[Int]("IntView"){ c =>
+      <.input(
+        ^.`type` := "number",
+        ^.value := c.model.toString,
+        ^.onChange ==> ((e: ReactEventI) => c.set(e.target.value.toInt))
+      )
+    }
 
-    val IntView =
-      ReactComponentB[Cursor[Int]]("IntView")
-        .render_P(c =>
-          <.input(
-            ^.`type` := "number",
-            ^.value := c.model.toString,
-            ^.onChange ==> ((e: ReactEventI) => c.set(e.target.value.toInt))
-          )
-        )
-        .configure(Reusability.shouldComponentUpdateWithOverlay)
-        .build
-
-    val StreetView =
-      ReactComponentB[Cursor[Street]]("StreetView")
-        .render_P(c =>
-          {
-            <.div(
-              <.div("Street: " + c.model.number + ", " + c.model.name),
-              IntView(c.zoom("number", Street.number)),
-              StringView(c.zoom("name", Street.name))
-            )
-          }
-        )
-        .build
+    val StreetView =view[Street]("StreetView") { c =>
+      <.div(
+        <.div("Street: " + c.model.number + ", " + c.model.name),
+        IntView(c.zoom("number", Street.number)),
+        StringView(c.zoom("name", Street.name))
+      )
+    }
 
     class AddressBackend(scope: BackendScope[Unit, Address]) {
 
@@ -112,17 +105,6 @@ object DemoApp extends JSApp {
       .renderBackend[AddressBackend]  // ← Use AddressBackend class and backend.render
       .build
 
-    // class StreetBackend($: BackendScope[Street, Unit]) {
-    //   def render(s: Street) =
-    //   <.div("Street: " + s.number + ", " + s.name)
-    // }
-    // 
-    // val StreetView = ReactComponentB[Unit]("Example")
-    //   .initialState(a)
-    //   .renderBackend[AddressBackend]  // ← Use Backend class and backend.render
-    //   .build
-
-
     val HelloMessage = ReactComponentB[String]("HelloMessage")
       .render($ => <.div("Hello ", $.props))
       .build
@@ -132,35 +114,4 @@ object DemoApp extends JSApp {
     ReactDOM.render(AddressView(), mountNode)
   }
 
-//   def appendPar(targetNode: dom.Node, text: String): Unit = {
-//     targetNode.appendChild(p(text).render)
-//   }
-// 
-//   def main(): Unit = {
-//     
-//     val a = Address(Street("OLD STREET", 1))
-// 
-//     //Build a delta using cursor, just adding resulting new models to paragraphs
-//     //alongside the encoded JSON, then applying the delta read from encoded js
-//     //and comparing the result, to show that server-side replication of the
-//     //delta would work
-//     val callback = (delta: Delta[Address], deltaJs: Js.Value) => {
-//       val aDelta = delta.apply(a)
-//       appendPar(document.body, "After cursor delta applied directly: " + aDelta)
-//       appendPar(document.body, "Cursor delta encoded as: " + deltaJs)
-//       
-//       val aDeltaJs = Address.addressDeltaReader.readDelta(deltaJs).apply(a)
-//       appendPar(document.body, "After cursor delta applied using JSON: " + aDeltaJs)
-//       
-//       appendPar(document.body, "Equal? " + (aDelta == aDeltaJs))
-//       
-//     }
-//     val root = RootParent(callback)
-//     val addressCursor = Cursor(root, a)
-//     val streetCursor = addressCursor.zoom("street", Address.street)
-//     
-//     streetCursor.set(Street("New street set using cursor", 9002))
-//     streetCursor.set(Street("Another new street set using cursor", 9003))
-//     streetCursor.act(StreetActionNumberMultiple(3))
-//   }
 }
