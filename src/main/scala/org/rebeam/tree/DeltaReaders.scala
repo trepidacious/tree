ba14901330d711fd65b5170aa97d8adaca612839
trepidacious <trepidacious@gmail.com>
2016-07-31 11:52:52 +0100

    Add more implicit delta readers for values

--- src/main/scala/org/rebeam/tree/DeltaReaders.scala
+++ src/main/scala/org/rebeam/tree/DeltaReaders.scala
@@ -14,5 +14,11 @@ object DeltaReaders {
     def readDelta(v: Js.Value): Delta[M] = pf.applyOrElse(v, (v: Js.Value) => throw Invalid.Data(v, error))
   }
   implicit val stringDeltaReader = deltaReaderFromReader[String]
+  implicit val booleanDeltaReader = deltaReaderFromReader[Boolean]
+  implicit val byteDeltaReader = deltaReaderFromReader[Byte]
+  implicit val shortDeltaReader = deltaReaderFromReader[Short]
   implicit val intDeltaReader = deltaReaderFromReader[Int]
+  implicit val longDeltaReader = deltaReaderFromReader[Long]
+  implicit val floatDeltaReader = deltaReaderFromReader[Float]
+  implicit val doubleDeltaReader = deltaReaderFromReader[Double]
 }
