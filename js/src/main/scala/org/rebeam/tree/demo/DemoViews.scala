30245845a541863bba548089d01d43b32358f7b6
trepidacious <trepidacious@gmail.com>
2016-09-04 14:34:49 +0100

    Refactor demo views to own object

---
+++ js/src/main/scala/org/rebeam/tree/demo/DemoViews.scala
@@ -0,0 +1,30 @@
+package org.rebeam.tree.demo
+
+import japgolly.scalajs.react.vdom.prefix_<^._
+import org.rebeam.tree.view.View._
+import org.rebeam.tree.view.WSRootComponent
+
+object DemoViews {
+
+  val StreetView = cursorView[Street]("StreetView") { c =>
+    <.div(
+      <.div("Street: " + c.model.number + ", " + c.model.name),
+      IntView(c.zoom("number", Street.number).label("Number")),
+      TextView(c.zoom("name", Street.name).label("Name")),
+      ActButton("Number multiple", c.act(StreetAction.NumberMultiple(10))),
+      ActButton("Capitalise", c.act(StreetAction.Capitalise))
+    )
+  }
+
+  val noData = <.div(<.p("Retrieving data from server..."))
+  val AddressView = WSRootComponent[Address](noData, "api/address") {
+    addressCursor => {
+      val streetCursor = addressCursor.zoom("street", Address.street)
+      <.div(
+        <.h1("Address"),
+        StreetView(streetCursor)
+      )
+    }
+  }
+  
+}
--- js/src/main/scala/org/rebeam/tree/demo/JSDemoApp.scala
+++ js/src/main/scala/org/rebeam/tree/demo/JSDemoApp.scala
@@ -1,45 +1,15 @@
 package org.rebeam.tree.demo
 
 import japgolly.scalajs.react._
-import japgolly.scalajs.react.vdom.prefix_<^._
-import org.rebeam.tree.view._
 import org.scalajs.dom
-import upickle.default._
 
-import scala.language.higherKinds
 import scala.scalajs.js.JSApp
-
-import org.rebeam.tree.view.View._
 
 object JSDemoApp extends JSApp {
 
   def main(): Unit = {
-
-    val StreetView = cursorView[Street]("StreetView") { c =>
-      <.div(
-        <.div("Street: " + c.model.number + ", " + c.model.name),
-        IntView(c.zoom("number", Street.number).label("Number")),
-        TextView(c.zoom("name", Street.name).label("Name")),
-        ActButton("Number multiple", c.act(StreetAction.NumberMultiple(10))),
-        ActButton("Capitalise", c.act(StreetAction.Capitalise))
-      )
-    }
-
-    val noData = <.div(<.p("Retrieving data from server..."))
-
-    val AddressView = WSRootComponent[Address](noData, "api/address") {
-      addressCursor => {
-        val streetCursor = addressCursor.zoom("street", Address.street)
-        <.div(
-          <.h1("Address"),
-          StreetView(streetCursor)
-        )
-      }
-    }
-
     val mountNode = dom.document.getElementsByClassName("content")(0)
-
-    ReactDOM.render(AddressView, mountNode)
+    ReactDOM.render(DemoViews.AddressView, mountNode)
   }
 
 }
