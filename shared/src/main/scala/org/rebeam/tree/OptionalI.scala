c58bdc7b413547ee119378e263d538d4dd55e6ad
trepidacious <trepidacious@gmail.com>
2016-10-11 15:28:13 +0100

    Remove copy/pasted docs - we are just delegating

--- shared/src/main/scala/org/rebeam/tree/OptionalI.scala
+++ shared/src/main/scala/org/rebeam/tree/OptionalI.scala
@@ -9,18 +9,13 @@ import scalaz.{Applicative, \/}
 case class OptionalI[A](index: Int) extends POptional[List[A], List[A], A, A] {
   private val o = listIndex[A].index(index)
 
-  /** get the target of a [[POptional]] or return the original value while allowing the type to change if it does not match */
   def getOrModify(s: List[A]): List[A] \/ A = o.getOrModify(s)
 
-  /** get the modified source of a [[POptional]] */
   def set(b: A): List[A] => List[A] = o.set(b)
 
-  /** get the target of a [[POptional]] or nothing if there is no target */
   def getOption(s: List[A]): Option[A] = o.getOption(s)
 
-  /** modify polymorphically the target of a [[POptional]] with an Applicative function */
   def modifyF[F[_]: Applicative](f: A => F[A])(s: List[A]): F[List[A]] = o.modifyF(f)(s)
 
-  /** modify polymorphically the target of a [[POptional]] with a function */
   def modify(f: A => A): List[A] => List[A] = o.modify(f)
 }
--- shared/src/main/scala/org/rebeam/tree/OptionalMatch.scala
+++ shared/src/main/scala/org/rebeam/tree/OptionalMatch.scala
@@ -21,18 +21,13 @@ case class OptionalMatch[A, F <: A => Boolean](f: F) extends POptional[List[A], 
     }
   )
 
-  /** get the target of a [[POptional]] or return the original value while allowing the type to change if it does not match */
   def getOrModify(s: List[A]): List[A] \/ A = o.getOrModify(s)
 
-  /** get the modified source of a [[POptional]] */
   def set(b: A): List[A] => List[A] = o.set(b)
 
-  /** get the target of a [[POptional]] or nothing if there is no target */
   def getOption(s: List[A]): Option[A] = o.getOption(s)
 
-  /** modify polymorphically the target of a [[POptional]] with an Applicative function */
   def modifyF[G[_]: Applicative](f: A => G[A])(s: List[A]): G[List[A]] = o.modifyF(f)(s)
 
-  /** modify polymorphically the target of a [[POptional]] with a function */
   def modify(f: A => A): List[A] => List[A] = o.modify(f)
 }
