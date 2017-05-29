b1e988c276710e6e83bf00047a9a507dd9244ffc
trepidacious <trepidacious@gmail.com>
2017-05-29 20:33:00 +0100

    Implement (most?) ref updating
    We now keep incoming and outgoing ref graph and refs themselves up to
    date in the cache when data items are updated (covering adding new data
    and updating existing data).
    More tests for this.

--- shared/src/test/scala/org/rebeam/tree/sync/RefSpec.scala
+++ shared/src/test/scala/org/rebeam/tree/sync/RefSpec.scala
@@ -1,6 +1,7 @@
 package org.rebeam.tree.sync
 
 import io.circe.generic.JsonCodec
+import org.rebeam.lenses.PrismN
 import org.rebeam.lenses.macros.Lenses
 import org.rebeam.tree.Delta._
 import org.rebeam.tree.DeltaCodecs._
@@ -15,7 +16,15 @@ import org.rebeam.tree.sync.Sync.Ref._
 
 class RefSpec extends WordSpec with Matchers with Checkers {
 
-  case class SimpleCacheState[A](data: A, revision: Long, incomingRefs: Set[Guid[_]], outgoingRefs: Set[Guid[_]])
+  case class SimpleCacheState[A](data: A, revision: Long, incomingRefs: Set[Guid[_]], outgoingRefs: Set[Guid[_]]) {
+    def updatedIncomingRefs(id: Guid[_], add: Boolean): SimpleCacheState[A] = copy(
+      incomingRefs = if (add) {
+        incomingRefs + id
+      } else {
+        incomingRefs - id
+      }
+    )
+  }
 
   class SimpleCache[M](private val map: Map[Guid[_], SimpleCacheState[_]])(implicit dCodecM: DeltaCodec[M]) extends Cache {
 
@@ -28,15 +37,117 @@ class RefSpec extends WordSpec with Matchers with Checkers {
 
     private def getState[A](id: Guid[A]): Option[SimpleCacheState[A]] = map.get(id).map(_.asInstanceOf[SimpleCacheState[A]])
 
-    private def get[A](id: Guid[A]): Option[A] = getState(id).map(_.data)
+    def incomingRefs[A](id: Guid[A]): Set[Guid[_]] = getState(id).map(_.incomingRefs).getOrElse(Set.empty[Guid[_]])
+    def outgoingRefs[A](id: Guid[A]): Set[Guid[_]] = getState(id).map(_.outgoingRefs).getOrElse(Set.empty[Guid[_]])
 
-    //TODO handle refsTo and From
+    def get[A](id: Guid[A]): Option[A] = getState(id).map(_.data)
 
-    //TODO update new entry?
-    def updated[A <: M](id: Guid[A], a: A): SimpleCache[M] = {
-      val updatedRev = getState(id).map(_.revision + 1).getOrElse(0L)
+    private def incomingRefsFor(id: Guid[_]): Set[Guid[_]] = {
+      map.foldLeft(Set.empty[Guid[_]]){
+        case (refs, entry) =>
+          // If the entry represents data having an outgoing ref to our id, then
+          // add that data's id to the incoming refs.
+          // Ignore the outgoing refs of our own data
+          if (entry._2.outgoingRefs.contains(id) && entry._1 != id) {
+            refs + entry._1
+          // No outgoing ref from the entry's data to our id, so leave refs unaltered
+          } else {
+            refs
+          }
+      }
+    }
+
+    private def updateIncomingRefs(id: Guid[_], outgoingRefs: Set[Guid[_]], add: Boolean): SimpleCache[M] = {
+      val map2 = outgoingRefs.foldLeft(map){
+        case (m, outgoingRef) =>
+          m.get(outgoingRef).fold {
+            // If data reached by outgoing ref is not in cache, nothing to update
+            m
+          }{
+            // If data IS in the cache, we update its cache state to add/remove ourselves as an incoming ref
+            (otherCacheState: SimpleCacheState[_]) =>
+              m.updated(
+                outgoingRef,
+                otherCacheState.updatedIncomingRefs(id, add)
+              )
+          }
+      }
+      new SimpleCache[M](map2)
+    }
+
+    def updated[A <: M](id: Guid[A], a: A): SimpleCache[M] = getState(id).fold {
+      // TODO we can use the same map2 and updateIncomingRefs calls in both cases
+      // if we produce the right values for updatedRev, previous/new outgoing refs
+      // and previous incoming refs
+
+      // We are adding new data - start from revision 0
+      val updatedRev = 0L
+
+      // Update refs in the added data
       val rur = dCodecM.updateRefs(RefUpdateResult.noRefs(a), this)
-      new SimpleCache(map.updated(id, SimpleCacheState(rur.data, updatedRev, Set.empty, rur.outgoingRefs)))
+
+      // Data is new to the cache, so we need to update incoming refs from scratch,
+      // and we need to add the new data to the incoming refs of all referenced data
+      // that is actually in the cache
+      val incomingRefs = incomingRefsFor(id)
+      val map2 = map.updated(id, SimpleCacheState(rur.data, updatedRev, incomingRefs, rur.outgoingRefs))
+      val cache2 = new SimpleCache[M](map2).updateIncomingRefs(id, rur.outgoingRefs, add = true)
+
+      // Finally, we need to update the refs in everything that points at us.
+      // This doesn't trigger any further updates, since the actual data in those
+      // data items that point at us hasn't changed.
+      incomingRefs.foldLeft(cache2){
+        case (c, incomingId) =>
+          c.getState(incomingId).fold(
+            c
+          )(
+            state => {
+              val rur = dCodecM.updateRefs(RefUpdateResult.noRefs(state.data.asInstanceOf[M]), c)
+              new SimpleCache[M](c.map.updated(incomingId, state.copy(data = rur.data)))
+            }
+          )
+      }
+
+    }{
+      // We are updating existing data
+      previousState => {
+        val updatedRev = previousState.revision + 1
+        // Update refs in the updated data
+        val updateResult = dCodecM.updateRefs(RefUpdateResult.noRefs(a), this)
+
+        // Update the map with the data having updated refs, and new revision.
+        // The incoming refs to this data have not changed since no other data has changed, so reuse from
+        // current state.
+        // The outgoing refs are as we just found.
+        val incomingRefs = previousState.incomingRefs
+        val map2 = map.updated(id, SimpleCacheState(updateResult.data, updatedRev, incomingRefs, updateResult.outgoingRefs))
+
+        // Update the incoming refs of everything else in the cache. We can optimise by looking
+        // at those outgoing refs that have been added to this data, and adding the corresponding
+        // incoming references for the referenced data. Then similarly for outgoing references
+        // that have been removed.
+        val previousOutgoingRefs = previousState.outgoingRefs
+        val newOutgoingRefs = updateResult.outgoingRefs
+        val cache2 = new SimpleCache[M](map2)
+          .updateIncomingRefs(id, newOutgoingRefs -- previousOutgoingRefs, add = true)
+          .updateIncomingRefs(id, previousOutgoingRefs -- newOutgoingRefs, add = false)
+
+        // Finally, we need to update the refs in everything that points at us.
+        // This doesn't trigger any further updates, since the actual data in those
+        // data items that point at us hasn't changed.
+        incomingRefs.foldLeft(cache2){
+          case (c, incomingId) =>
+            c.getState(incomingId).fold(
+              c
+            )(
+              state => {
+                val rur = dCodecM.updateRefs(RefUpdateResult.noRefs(state.data.asInstanceOf[M]), c)
+                new SimpleCache[M](c.map.updated(incomingId, state.copy(data = rur.data)))
+              }
+            )
+        }
+
+      }
     }
 
     def updated[A <: M](a: A)(implicit toId: ToId[A]): SimpleCache[M] = updated(toId.id(a), a)
@@ -67,25 +178,32 @@ class RefSpec extends WordSpec with Matchers with Checkers {
     @JsonCodec
     @Lenses
     case class Tag(id: Guid[Tag], tag: String) extends HasId[Tag] with Data
+
+    val addressPrism: PrismN[Data, Address] = PrismN.classTag("Address")
+    val userPrism: PrismN[Data, User] = PrismN.classTag("User")
+    val postPrism: PrismN[Data, Post] = PrismN.classTag("Post")
+    val tagPrism: PrismN[Data, Tag] = PrismN.classTag("Tag")
   }
 
   import Data._
 
-  implicit lazy val addressDeltaDecoder =
+  implicit lazy val addressDeltaDecoder: DeltaCodec[Address] =
     DeltaCodecs.value[Address] or lensN(Address.streetName) or lensN(Address.number)
 
-  implicit lazy val userDeltaDecoder =
+  implicit lazy val userDeltaDecoder: DeltaCodec[User] =
     DeltaCodecs.value[User] or lensN(User.name) or lensN(User.addressRef)
 
   implicit lazy val postDeltaDecoder: DeltaCodec[Post] =
     DeltaCodecs.value[Post] or lensN(Post.message) or lensN(Post.userRef) or lensN(Post.tagRefs)
 
-  implicit lazy val tagDeltaDecoder =
+  implicit lazy val tagDeltaDecoder: DeltaCodec[Tag] =
     DeltaCodecs.value[Tag] or lensN(Tag.tag)
 
-  implicit lazy val listTagRefsDecoder = DeltaCodecs.optionalI[Ref[Tag]]
+  implicit lazy val listTagRefsDecoder: DeltaCodec[List[Ref[Tag]]] =
+    DeltaCodecs.optionalI[Ref[Tag]]
 
-  implicit lazy val dataDeltaCodec = DeltaCodecs.value[Data]
+  implicit lazy val dataDeltaCodec: DeltaCodec[Data] =
+    prismN(Data.postPrism) or prismN(Data.addressPrism) or prismN(Data.userPrism) or prismN(Data.tagPrism)
 
 
   val examplePostIO: DeltaIO[(Post, SimpleCache[Data])] = for {
@@ -95,19 +213,57 @@ class RefSpec extends WordSpec with Matchers with Checkers {
     tagId1 <- getId[Tag]
     tagId2 <- getId[Tag]
   } yield {
-    val post = Post(postId, "Hello World!", Ref(userId), List(Ref(tagId1), Ref(tagId2)))
+    val post = Post(
+      id = postId,
+      message = "Hello World!",
+      userRef = Ref(userId),
+      tagRefs = List(Ref(tagId1), Ref(tagId2)))
+    val cache = SimpleCache.empty[Data]
+      .updated(Address(addressId, "Street Name", 42))
+      .updated(User(userId, "User", Ref(addressId)))
+      .updated(post)
+      .updated(Tag(tagId1, "Tag1"))
+      .updated(Tag(tagId2, "Tag2"))
     (
-      post,
-      SimpleCache.empty[Data]
-        .updated(Address(addressId, "Street Name", 42))
-        .updated(User(userId, "User", Ref(addressId)))
-        .updated(post)
-        .updated(Tag(tagId1, "Tag1"))
-        .updated(Tag(tagId2, "Tag2"))
+      //FIXME get
+      cache.get(postId).get,
+      cache
     )
   }
 
   val examplePost: (Post, SimpleCache[Data]) = DeltaIORun.runDeltaIO(examplePostIO, DeltaIOContext(Moment(100)), DeltaId(ClientId(1), ClientDeltaId(1)))
+
+  "SimpleCache" should {
+    "produce correct refs graph" in {
+      val cache = examplePost._2
+      val post = examplePost._1
+
+      assert(cache.incomingRefs(post.id) == Set.empty)
+      assert(cache.outgoingRefs(post.id) == Set(post.userRef.guid) ++ post.tagRefs.map(_.guid))
+
+      val r = for {
+        user <- cache(post.userRef)
+        address <- cache(user.addressRef)
+        tag1 <- cache(post.tagRefs.head)
+        tag2 <- cache(post.tagRefs(1))
+      } yield {
+        assert(cache.incomingRefs(user.id) == Set(post.id))
+        assert(cache.outgoingRefs(user.id) == Set(address.id))
+
+        assert(cache.incomingRefs(address.id) == Set(user.id))
+        assert(cache.outgoingRefs(address.id) == Set())
+
+        assert(cache.incomingRefs(tag1.id) == Set(post.id))
+        assert(cache.outgoingRefs(tag1.id) == Set())
+
+        assert(cache.incomingRefs(tag2.id) == Set(post.id))
+        assert(cache.outgoingRefs(tag2.id) == Set())
+        true
+      }
+
+      assert(r.isDefined)
+    }
+  }
 
   "DeltaCodecs" should {
     "update refs" in {
@@ -120,21 +276,33 @@ class RefSpec extends WordSpec with Matchers with Checkers {
         message = post.message,
         userRef = RefResolved(post.userRef.guid, 0),
         tagRefs = List(
-          RefResolved(post.tagRefs(0).guid, 0),
+          RefResolved(post.tagRefs.head.guid, 0),
           RefResolved(post.tagRefs(1).guid, 0)
         )
       )
 
       assert(postUpdated == postExpected)
-      println(postUpdated)
+    }
+
+    "find outgoing refs" in {
+      val post = examplePost._1
+      val rur = postDeltaDecoder.updateRefs(RefUpdateResult.noRefs(post), examplePost._2)
+
+      assert(rur.outgoingRefs == Set(post.userRef.guid) ++ post.tagRefs.map(_.guid))
+    }
+
+    "find outgoing refs via prism" in {
+      val post = examplePost._1
+      val rur = dataDeltaCodec.updateRefs(RefUpdateResult.noRefs(post), examplePost._2)
+      assert(rur.outgoingRefs == Set(post.userRef.guid) ++ post.tagRefs.map(_.guid))
     }
 
     "update refs incrementally" in {
       val post = examplePost._1
       val cache = examplePost._2
 
-      //Check the un-updated post's user ref doesn't get data
-      assert(cache(post.userRef).isEmpty)
+//      //Check the un-updated post's user ref doesn't get data
+//      assert(cache(post.userRef).isEmpty)
 
       //Update the post's refs so they have revisions
       val postUpdated = postDeltaDecoder.updateRefsDataOnly(examplePost._1, examplePost._2)
