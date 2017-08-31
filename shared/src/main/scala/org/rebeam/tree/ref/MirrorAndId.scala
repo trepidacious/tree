5de987f0f971a576f9c3fb2a510e9786e05aa221
trepidacious <trepidacious@gmail.com>
2017-08-31 09:42:02 +0100

    Add constructor accepting delta

--- shared/src/main/scala/org/rebeam/tree/ref/MirrorAndId.scala
+++ shared/src/main/scala/org/rebeam/tree/ref/MirrorAndId.scala
@@ -3,6 +3,7 @@ package org.rebeam.tree.ref
 import org.rebeam.lenses.macros.Lenses
 import org.rebeam.tree.sync.Sync._
 import org.rebeam.tree.DeltaCodecs._
+import org.rebeam.tree.Delta._
 import scala.language.higherKinds
 import io.circe._, io.circe.generic.semiauto._
 
@@ -17,4 +18,8 @@ object MirrorAndId {
   implicit def mirrorAndIdDeltaCodec[M](implicit mirrorDeltaCodec: DeltaCodec[Mirror], mDeltaCodec: DeltaCodec[M]): DeltaCodec[MirrorAndId[M]] = lensN(MirrorAndId.mirror)
   implicit def mirrorAndIdDecoder[M](implicit mirrorDecoder: Decoder[Mirror]): Decoder[MirrorAndId[M]] = deriveDecoder[MirrorAndId[M]]
   implicit def mirrorAndIdEncoder[M](implicit mirrorEncoder: Encoder[Mirror]): Encoder[MirrorAndId[M]] = deriveEncoder[MirrorAndId[M]]
+
+  def apply[M: ToId](d: DeltaIO[M]): DeltaIO[MirrorAndId[M]] = for {
+    m <- d
+  } yield MirrorAndId(Mirror.empty, implicitly[ToId[M]].id(m))
 }
