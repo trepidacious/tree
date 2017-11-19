d402797f149bcffc59b5f4e10d36c9fe6974f8fc
trepidacious <trepidacious@gmail.com>
2017-11-19 08:39:45 +0000

    Add Searchable, should be useful for finding Refs in data

---
+++ shared/src/main/scala/org/rebeam/tree/Searchable.scala
@@ -0,0 +1,94 @@
+package org.rebeam.tree
+
+import scala.language.implicitConversions
+
+import shapeless._
+
+// Evidence that an A is something that we can look around in for Qs that
+// satisfy some predicate, where we will collect all found Q's in a Set
+trait Searchable[A, Q] {
+  def find(p: Q => Boolean)(a: A): Set[Q]
+}
+
+trait LowPrioritySearchable {
+  implicit def hlistishSearchable[A, L <: HList, Q](
+    implicit gen: Generic.Aux[A, L], s: Searchable[L, Q]
+  ): Searchable[A, Q] = new Searchable[A, Q] {
+    def find(p: Q => Boolean)(a: A) = s.find(p)(gen to a)
+  }
+
+  import Searchable.notSearchable
+
+  // Provide low-priority searchables for values, which will generally
+  // only be searched for directly, rather than recursively (as we do for 
+  // lists, products etc.). When Q is one of these types, elemSearchable will
+  // be found as a higher priority implicit, and this will apply the predicate
+  // directly to the value.
+  implicit def booleanSearchable[Q]: Searchable[Boolean, Q] = notSearchable
+  implicit def byteSearchable[Q]: Searchable[Byte, Q] = notSearchable
+  implicit def shortSearchable[Q]: Searchable[Short, Q] = notSearchable
+  implicit def intSearchable[Q]: Searchable[Int, Q] = notSearchable
+  implicit def longSearchable[Q]: Searchable[Long, Q] = notSearchable
+  implicit def floatSearchable[Q]: Searchable[Float, Q] = notSearchable
+  implicit def doubleSearchable[Q]: Searchable[Double, Q] = notSearchable
+  implicit def charSearchable[Q]: Searchable[Char, Q] = notSearchable
+  implicit def stringSearchable[Q]: Searchable[String, Q] = notSearchable
+  implicit def symbolSearchable[Q]: Searchable[Symbol, Q] = notSearchable
+}
+
+object Searchable extends LowPrioritySearchable {
+
+  /**
+    * Defines a Searchable that finds nothing
+    */
+  def notSearchable[A, Q]: Searchable[A, Q] = new Searchable[A, Q] {
+    def find(p: Q => Boolean)(a: A) = Set.empty
+  }
+
+  // This ensures that the actual type we are searching for is used directly with predicate,
+  // this overrides the low priority implementations if we are looking for e.g. a String.
+  implicit def elemSearchable[A]: Searchable[A, A] = new Searchable[A, A] {
+    def find(p: A => Boolean)(a: A) = if (p(a)) Set(a) else Set.empty
+  }
+
+  implicit def listSearchable[A, Q](implicit s: Searchable[A, Q]): Searchable[List[A], Q] =
+    new Searchable[List[A], Q] {
+      def find(p: Q => Boolean)(a: List[A]) = a.flatMap(s.find(p)).toSet
+    }
+
+  implicit def setSearchable[A, Q](implicit s: Searchable[A, Q]): Searchable[Set[A], Q] =
+    new Searchable[Set[A], Q] {
+      def find(p: Q => Boolean)(a: Set[A]) = a.flatMap(s.find(p)).toSet
+    }
+
+  implicit def hnilSearchable[Q]: Searchable[HNil, Q] = new Searchable[HNil, Q] {
+    def find(p: Q => Boolean)(a: HNil) = Set.empty
+  }
+
+  implicit def hlistSearchable[H, T <: HList, Q](
+    implicit hs: Searchable[H, Q], ts: Searchable[T, Q]
+  ): Searchable[H :: T, Q] = new Searchable[H :: T, Q] {
+    def find(p: Q => Boolean)(a: H :: T) =
+      hs.find(p)(a.head) ++ ts.find(p)(a.tail)
+      // If we want to allow compilation with missing typeclasses for types occurring in HLists, we
+      // can add a default value of null to hs parameter above, and then use the less safe 
+      // version below.
+      // However we then risk missing data we SHOULD be searching. The current form requires
+      // a typeclass for all types in the data, this can be easily provided for unsearchable
+      // types using:
+      // ```
+      // implicit def someTypeSearchable[Q]: Searchable[SomeType, Q] = notSearchable
+      // ```
+      // The less safe version:
+      // If we have a searchable for head, use it, and add the results of searching the tail
+      // Option(hs).fold(Set.empty[Q])(_.find(p)(a.head)) ++ ts.find(p)(a.tail)
+  }
+
+  case class SearchableWrapper[A](a: A) {
+    def deepFind[Q](p: Q => Boolean)(implicit s: Searchable[A, Q]) =
+      s.find(p)(a)
+  }
+
+  implicit def wrapSearchable[A](a: A): SearchableWrapper[A] = SearchableWrapper(a)
+
+}
---
+++ shared/src/main/scala/org/rebeam/tree/demo/SearchableDemo.scala
@@ -0,0 +1,40 @@
+package org.rebeam.tree.demo
+
+import org.rebeam.tree.Searchable._
+
+object SearchableDemo extends App {
+
+  // An example predicate:
+  val p = (_: String) endsWith "o"
+
+  // On strings:
+  assert("hello".deepFind(p) == Set("hello"))
+  assert("hell".deepFind(p) == Set.empty[String])
+
+  // On lists:
+  assert(List("yes", "maybe", "no").deepFind(p) == Set("no"))
+
+  // On arbitrarily sized and nested tuples:
+  assert(("yes", "maybe", ("no", "why")).deepFind(p) == Set("no"))
+  assert(("a", ("b", "c"), "d").deepFind(p) == Set.empty[String])
+
+  // On tuples with non-string elements:
+  assert((1, "two", ('three, '4')).deepFind(p) == Set("two"))
+
+  // Search the same tuple for a specific character instead:
+  assert((1, "two", ('three, '4')).deepFind((_: Char) == '4') == Set('4'))
+
+  // Our case class:
+  case class Foo(a: String, b: String, c: List[String])
+
+  // And it works:
+  assert(Foo("four", "three", List("two", "one")).deepFind(p) == Set("two"))
+  assert(Foo("a", "b", "c" :: Nil).deepFind(p) == Set.empty[String])
+
+  println(Foo("fouro", "three", List("two", "one")).deepFind(p))
+
+  println(Some("fouro").deepFind(p))
+
+  println(Set("fouro").deepFind(p))
+
+}
